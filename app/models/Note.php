a70edfcb903ef057ccb0ff7722dc089933b42c28
Yaroslav <theyosaman@yandex.ru>
2018-06-27 12:58:03 +0300

    /

--- app/models/Note.php
+++ app/models/Note.php
@@ -14,28 +14,4 @@ class Note extends Model
     public $date;
     public $user_id;
 
-
-    public function printPost(){
-        echo '<h3>' . $this->title . '</h3>';
-        echo '<p>' . $this->note . '</p>';
-        echo '<p>' . $this->data . '</p>';
-    }
-
-    public function printPost2(){
-        echo '<div id="' . $this->id . '" class="note col-md-4">';
-        echo '<p class="date">' . $this->date . '</p>';
-        echo '<h2>' . $this->title . '</h2>';
-        echo '<p class="noteText">' . $this->note . '</p>';
-        echo '<button data-id=' . $this->id . 'type="button" class="btn btn-block btn-primary">More</button></div>';
-    }
-
-
-    public function editPost(){
-        echo '<input name="title" value="' . $this->title .  '">';
-        echo '<input name="content" value="' . $this->date . '">';
-        echo '<input name="id" type="hidden" value="' . $this->id. '">';
-        echo '<br>';
-        echo '<button>Save</button>';
-    }
-
 }
